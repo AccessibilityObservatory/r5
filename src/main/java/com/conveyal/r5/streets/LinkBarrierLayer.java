63c2b5cb3c5ab804fa8712eed828b577703ae9f0
Andrew Owen <owenx148@umn.edu>
2024-06-10 22:49:45 -0500

    Load LinkBarrierLayer from shapefile

--- src/main/java/com/conveyal/r5/streets/LinkBarrierLayer.java
+++ src/main/java/com/conveyal/r5/streets/LinkBarrierLayer.java
@@ -1,9 +1,20 @@
 package com.conveyal.r5.streets;
 
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
 import org.locationtech.jts.geom.*;
 import org.locationtech.jts.index.strtree.STRtree;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.conveyal.r5.streets.VertexStore.fixedDegreesToFloating;
 
@@ -11,24 +22,40 @@ import static com.conveyal.r5.streets.VertexStore.fixedDegreesToFloating;
  * A geometry layer that blocks links between origins/destinations and a StreetLayer.
  */
 public class LinkBarrierLayer {
-    private GeometryFactory geomFactory;
-    private STRtree featureIndex;
+    private final GeometryFactory geomFactory;
+    private final STRtree featureIndex;
 
-    public LinkBarrierLayer(GeometryCollection geoms) {
+    public LinkBarrierLayer(FeatureCollection<SimpleFeatureType, SimpleFeature> collection) {
         geomFactory = new GeometryFactory();
         featureIndex = new STRtree();
         // Build index of geometries to speed up intersection testing
-        for (GeometryCollectionIterator it = new GeometryCollectionIterator(geoms); it.hasNext(); ) {
-            Geometry g = (Geometry) it.next();
-            featureIndex.insert(g.getEnvelopeInternal(), g);
+        try (FeatureIterator<SimpleFeature> it = collection.features()) {
+            while (it.hasNext()) {
+                SimpleFeature feature = it.next();
+                Envelope env = ((Geometry) feature.getDefaultGeometry()).getEnvelopeInternal();
+                featureIndex.insert(env, feature);
+            }
         }
+    }
 
+    public static LinkBarrierLayer fromShapefile(String path) throws IOException {
+        File file = new File(path);
+        Map<String, Object> map = new HashMap<>();
+        map.put("url", file.toURI().toURL());
+
+        DataStore dataStore = DataStoreFinder.getDataStore(map);
+        String typeName = dataStore.getTypeNames()[0];
+        FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
+        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures();
+
+        return new LinkBarrierLayer(collection);
     }
 
     boolean intersects(Geometry q) {
-        List<Geometry> candidates = featureIndex.query(q.getEnvelopeInternal());
-        for (Geometry g: candidates) {
-            if (g.intersects(q))
+        List<SimpleFeature> candidates = featureIndex.query(q.getEnvelopeInternal());
+        for (SimpleFeature f: candidates) {
+            Geometry geom = (Geometry) f.getDefaultGeometry();
+            if (geom.intersects(q))
                 return true;
         }
         return false;
